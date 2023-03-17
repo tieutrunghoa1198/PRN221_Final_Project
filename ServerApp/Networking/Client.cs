8d1a054b2b7c2e71c4478a54cb7c4a03061ed7cd
Tiêu Trung Hoà <tieutrunghoa1198@gmail.com>
2023-03-17 10:08:57 +0700

    Add project files.

---
+++ ServerApp/Networking/Client.cs
@@ -0,0 +1,52 @@
+﻿using ServerApp.Networking.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace ServerApp.Networking
+{
+    class Client
+    {
+        public string username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient _clientSocket { get; set; }
+        PacketReader _packetReader;
+        public Client(TcpClient clientSocket)
+        {
+            _clientSocket = clientSocket;
+            UID = Guid.NewGuid();
+            _packetReader = new PacketReader(_clientSocket.GetStream());
+            var opcode = _packetReader.ReadByte();
+            username= _packetReader.readMessage();
+
+            Console.WriteLine($"{DateTime.Now}: Client has connected with user name: {username} ");
+            Task.Run( () => Process());
+        }
+        public void Process() 
+        { 
+            while(true)
+            {
+                try
+                {
+                    var opcode = _packetReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 2:
+                            var msg = _packetReader.readMessage();
+                            Console.WriteLine($"{DateTime.Now} message received: {msg}");
+                            //Program.broadCastMessage($"[{DateTime.Now}] : [{username}] : {msg}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using ServerApp.Networking;
+using ServerApp.Networking.IO;
+using System.Net;
+using System.Net.Sockets;
+namespace ServerApp
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+        static Socket socketForServer;
+        static TcpClient _tcpClient;
+        private static Thread th_RunServer;
+        static void Main(string[] args)
+        {
+            StartListen();
+        }
+
+        private static void StartListen()
+        {
+            var port = 7777;
+            _users = new List<Client>();
+            _listener = new TcpListener(System.Net.IPAddress.Any, port);
+            _listener.Start();
+            Console.WriteLine("server is started: " + port);
+            while (true)
+            {
+                socketForServer = _listener.AcceptSocket();
+                Console.WriteLine(socketForServer);
+                _tcpClient = _listener.AcceptTcpClient();
+                th_RunServer = new Thread(new ThreadStart(HandleClientConnection));
+                th_RunServer.Start();
+            }
+        }
+
+        private static void HandleClientConnection()
+        {
+
+        }
+    }
+}
